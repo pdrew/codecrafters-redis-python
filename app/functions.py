07f5c359b3baf971cfd23e05c7bd11dd97733482
pdrew <pdrew@tessituranetwork.com>
2024-03-07 20:26:19 +1100

    Send handshake (2/3)

--- app/functions.py
+++ app/functions.py
@@ -49,6 +49,6 @@ def encode_bulk_string(value: list[str]) -> bytes:
     return f"${len(bulk_string)}\r\n{bulk_string}\r\n".encode(ENCODING)
 
 def encode_array(value: list[object]) -> bytes:
-    elements = ("\r\n").join([f"${len(s)}\r\n{s}" for s in value])
+    elements = ("\r\n").join([f"${len(str(s))}\r\n{s}" for s in value])
 
     return f"*{len(value)}\r\n{elements}\r\n".encode(ENCODING)

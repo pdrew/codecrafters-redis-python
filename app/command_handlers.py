04d2f5aa7626dc7e0245c7529a6d902838b936aa
pdrew <pdrew@tessituranetwork.com>
2024-03-07 17:34:43 +1100

    Expiry

--- app/command_handlers.py
+++ app/command_handlers.py
@@ -1,26 +1,40 @@
 from socket import socket
 from app.functions import encode_simple_string, encode_bulk_string
 from app.constants import ENCODING
+from time import time
 
 def handle_ping(socket: socket, args: list[bytes]) -> None:
-    encoded = encode_simple_string("PONG")
-    socket.sendall(encoded)
+    response = encode_simple_string("PONG")
+    socket.sendall(response)
 
 def handle_echo(socket: socket, args: list[bytes]) -> None:
     message = ' '.join([b.decode(ENCODING) for b in args])
-    encoded = encode_simple_string(message)
-    socket.sendall(encoded)
+    response = encode_simple_string(message)
+    socket.sendall(response)
 
 def handle_set(socket: socket, args: list[bytes], database: dict) -> None:
-    key = args[0].decode(ENCODING)
-    value =  ' '.join([b.decode(ENCODING) for b in args[1:]])
-    database[key] = value
-    encoded = encode_simple_string("OK")
-    socket.sendall(encoded)
+    key = args.pop(0).decode(ENCODING)
+    value = args.pop(0).decode(ENCODING)
+
+    expiry = (round(time() * 1000) + int(args[1].decode(ENCODING))) if args and args[0].upper() == b'PX' else None
+
+    database[key] = (value, expiry)
+    response = encode_simple_string("OK")
+    socket.sendall(response)
 
 def handle_get(socket: socket, args: list[bytes], database: dict) -> None:
     key = args[0].decode(ENCODING)
 
-    value = encode_simple_string(database[key]) if key in database else encode_bulk_string(None)
+    response = encode_bulk_string(None)
 
-    socket.sendall(value)
+    if key in database:
+        value, expires = database[key]
+
+        current = round(time() * 1000)
+
+        if expires and expires < current:
+            del database[key]
+        else:
+            response = encode_simple_string(value)
+    
+    socket.sendall(response)
